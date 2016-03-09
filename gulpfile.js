912add340a1ab956f8530c2afc50f5155c241dc1
nukkunaana <nuochi@yandex.ru>
2016-03-09 15:50:00 +0300

    add gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
+var nodemon = require('gulp-nodemon');
 
 var jsFiles = ['*.js', 'public/js/*.js'];
 
@@ -11,10 +12,19 @@ gulp.task('style', function(){
             verbose: true
         }))
         .pipe(jscs());
-});
+})
 
 gulp.task('inject', function(){
     var wiredep = require('wiredep').stream;
+    var inject = require('gulp-inject');
+    var injectScr = gulp.src([
+                                './public/css/*.css',
+                                './public/js/*.js'
+                            ], { read: false });
+    var injectOptions = {
+        ignorePath: '/public'
+    };
+
     var options = {
         bowerJson: require('./bower.json'),
         directory: './public/lib'
@@ -22,5 +32,21 @@ gulp.task('inject', function(){
 
     return gulp.src('./public/index.html')
                 .pipe(wiredep(options))
+                .pipe(inject(injectScr, injectOptions))
                 .pipe(gulp.dest('./public'));
-});
+})
+
+gulp.task('serve', ['inject'], function(){
+    var options = {
+        script: 'app.js',
+        delayTime: 1,
+        env: {
+            'PORT' : 8080
+        },
+        watch: jsFiles
+    };
+    return nodemon(options)
+            .on('restart', function(ev){
+                console.log('Restarting...');
+            })
+})
