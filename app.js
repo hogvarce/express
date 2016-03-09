32ce2a4b5c1c5b819e2c4b722fb621d62f13d7d5
nukkunaana <nuochi@yandex.ru>
2016-03-09 11:35:51 +0300

    firest commit

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+
+var app = express();
+
+var port = 5000;
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res){
+    res.send('hello');
+});
+
+app.listen(port, function(err){
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('Listen port: %d', port);
+    }
+});
